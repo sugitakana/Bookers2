26454d34145bedc2d1c410656e5a5ac318bfb33b
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-03-22 04:38:29 +0000

    [Add]詳細ページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,8 +12,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
 private
