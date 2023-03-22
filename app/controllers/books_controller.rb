e18f596816ae937431a187b70e1bc329dd0ef335
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-03-22 05:24:06 +0000

    [Add]編集ページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,13 +13,27 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
       render :index
     end
   end
-
-private
+    
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+  
+  private
 
   def book_params
     params.require(:book).permit(:title, :body)
