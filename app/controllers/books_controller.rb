d081d6b6c4eb20dfd368dddc565280ffe904f833
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-03-22 13:14:37 +0000

    [Add]ユーザー一覧ページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,14 @@
 class BooksController < ApplicationController
-  
+
   def index
     @book = Book.new
     @books = Book.all
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -18,11 +18,11 @@ class BooksController < ApplicationController
       render :index
     end
   end
-    
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -32,13 +32,13 @@ class BooksController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
