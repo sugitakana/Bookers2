91b182c65d22f5b55264db56392f9e572d07312b
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-03-24 10:03:32 +0000

    [Add]部分テンプレート

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+ before_action :is_matching_login_user, only: [:edit, :update]
+  
   def index
     @users = User.all
     @book = Book.new
@@ -29,5 +31,12 @@ private
 
   def user_params
     params.require(:user).permit(:name, :introduction)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
   end
 end
