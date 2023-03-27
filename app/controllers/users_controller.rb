29f1d14cc429353cb1d307b1070b500a5094c53f
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-03-27 15:01:44 +0000

    [Add]レイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
  before_action :is_matching_login_user, only: [:edit, :update]
-  
+
   def index
     @users = User.all
     @book = Book.new
@@ -32,11 +32,11 @@ private
   def user_params
     params.require(:user).permit(:name, :introduction)
   end
-  
+
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to books_path
+      redirect_to user_path(current_user)
     end
   end
 end
